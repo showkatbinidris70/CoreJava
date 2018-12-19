59840bff199735007f6b6aca63ed23280f1564a9
showkatbinidris70 <showkatbinidris70@gmail.com>
2018-12-19 20:01:03 +0600

    message

---
+++ BioDate/src/BioData.java
@@ -0,0 +1,9 @@
+
+public class BioData {
+    public static void main(String[] args) {
+        System.out.print("\'Showkat\'\n \t01921839593 \n");
+        System.out.println("Ali");
+        
+    }
+    
+}
---
+++ BioDate/src/CommentDemo.java
@@ -0,0 +1,6 @@
+
+public class CommentDemo {
+    public static void main(String[] args) {
+        System.out.println("Showkat Ali");
+    }
+}
